5b3551ce8c9b004892c20e98b32d9c47bd6aa6d5
leehaejoon <haejoon309@gmail.com>
2019-09-10 14:54:37 +0900

    Add codecov

--- run_test.py
+++ run_test.py
@@ -8,7 +8,8 @@ test_path = base_path + 'tests/*'
 
 def run_test(language):
     if language == PYTHON:
-        os.system('pytest {}'.format(test_path))
+        # os.system('pytest {}'.format(test_path))  # for only pytest
+        os.system('pytest --cov=./ ./{}'.format(test_path))  # pytest with codecoverage
 
 
 def main():
