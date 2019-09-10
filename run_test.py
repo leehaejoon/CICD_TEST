66077b25e3cf1dee11f33deee0ce777e7a4b4cbc
leehaejoon <haejoon309@gmail.com>
2019-09-10 14:57:52 +0900

    Fix src & test path

--- run_test.py
+++ run_test.py
@@ -4,12 +4,13 @@ import yaml
 PYTHON = 'python'
 base_path = 'ITI/'
 config_path = base_path + '.cicd.yaml'
+source_path = base_path + 'src/*'
 test_path = base_path + 'tests/*'
 
 def run_test(language):
     if language == PYTHON:
         # os.system('pytest {}'.format(test_path))  # for only pytest
-        os.system('pytest --cov=./ ./{}'.format(test_path))  # pytest with codecoverage
+        os.system('pytest --cov={} {}'.format(source_path, test_path))  # pytest with codecoverage
 
 
 def main():
